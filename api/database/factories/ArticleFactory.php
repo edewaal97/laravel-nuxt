1776761f00977936e576b804b633cfa07bf7f58d
edewaal97 <edewaal97@users.noreply.github.com>
2026-01-27 18:51:37 +0100

    Add article to API

---
+++ api/database/factories/ArticleFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = $this->faker->sentence();
+        return [
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'banner_image' => 'https://placehold.co/600x400',
+            'body' => $this->faker->paragraphs(3, true),
+            'created_at' => now(),
+        ];
+    }
+}
--- api/routes/api.php
+++ api/routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/articles', function () {
+    return Article::select('id', 'title', 'slug')->get()->toResourceCollection();
+})->name('articles.index');
+
+Route::get('/articles/{article:slug}', function (Article $article) {
+    return $article->toResource();
+})->name('articles.show');
