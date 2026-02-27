c114c20d9f92bf07e0ee51601ce3c4de39e4a895
edewaal97 <edewaal97@users.noreply.github.com>
2026-02-27 13:57:57 +0100

    Seed 10 example articles with the default seeder.

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Article;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,11 +18,12 @@ final class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Article::factory()->count(10)->create();
     }
 }
